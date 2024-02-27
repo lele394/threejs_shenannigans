c73e99f0967650eceea10fa8c39e216d829b1572
lele394 <lele394prod@gmail.com>
2024-02-28 00:35:27 +0100

    fixed path #2 for stats using CDN

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ console.log(render_settings);
 import { arrayEqual } from './src/tools.js';
 
 
-import Stats from './node_modules/stats.js/src/Stats.js';
+import Stats from 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/17/Stats.js'
+
 
 const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
